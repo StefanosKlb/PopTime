d843163445c645a2ef80f6ad5eca0cbae81f40c1
Stephen Kolovos <kolovosstef2709@gmail.com>
2024-08-10 00:53:32 +0300

    added Installable feature

--- src/main.ts
+++ src/main.ts
@@ -200,4 +200,17 @@ export function initializeEventListeners() {
 document.addEventListener("DOMContentLoaded", () => {
     loadMediaSections();
     initializeEventListeners();
+
+     if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js')
+                .then((registration) => {
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                })
+                .catch((error) => {
+                    console.log('ServiceWorker registration failed: ', error);
+                });
+        });
+    }
+
 });
